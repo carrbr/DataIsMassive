46754dd88382ae1f05d284ef5024973cc4a5da29
blacksun777 <blacksun777@users.noreply.github.com>
2015-04-24 20:16:33 +0800

    Bug: 'Corrected' verbose behavior of interactive Mode. now shuts up when there is nothing to say ^^

--- DataIsMassiv/src/ui/Main.java
+++ DataIsMassiv/src/ui/Main.java
@@ -32,40 +32,34 @@ public class Main {
 
 	}
 
-	private static void handleInput(String [] args) throws Exception {
-		Scanner s = new Scanner(System.in);
-		
-		// check if we are running a batch job from a script or an interactive, user job
-		if (args.length == 0) { // no args -> interactive, user job
-			// let the user perform as many actions as they like without having
-			// to restart the program every time
-			while (true) {
-				args = readInCommand(s);	
-				// does user want to quit?
-				if (args.length > 0 && args[0].equalsIgnoreCase("quit")) {
-					break;
-				}
-				new MainTaskDelegation(args).exec();
-			}
-		} else { // batch job with command line args
+	private static void handleInput(String[] args) throws Exception {
+		if (args.length == 0)
+			interactiveMode();
+		else
 			new MainTaskDelegation(args).exec();
-		}
-		
-		s.close();
 	}
 
-	private static String[] readInCommand(Scanner s) {
-		String command[] = null;
-		String line = null;
-		
-		System.out.println("Type '?' or 'help' to get help");
+	private static void interactiveMode() throws Exception {
 
-		if (s.hasNext()) {
-			line = s.nextLine();
+		System.out.println("Type '?' or 'help' to get help\n"
+				+ "Type 'quit' or 'q' to quit, who would have guessed?");
+
+		try (Scanner scan = new Scanner(System.in)) {
+			while (true) {
+				String[] args = readInCommand(scan);
+				if (args.length > 0 && args[0].toLowerCase().matches("quit|q"))
+					break;
+				new MainTaskDelegation(args).exec();
+			}
 		}
-		command = line.split(" ");
+	}
 
+	private static String[] readInCommand(Scanner scan) {
 
+		String command[] = null;
+		do {
+			command = scan.nextLine().split(" ");
+		} while (command.length == 0 || command[0].equals(""));
 		return command;
 	}
 
