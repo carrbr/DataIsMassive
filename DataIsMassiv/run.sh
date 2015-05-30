0e5e0b7e5f1dad8d856c03054d2af1b457680bbe
Brian Carr <briancarr@bcUBUNTU.(none)>
2015-05-29 19:07:02 -0700

    run.sh takes more args now

--- DataIsMassiv/run.sh
+++ DataIsMassiv/run.sh
@@ -6,13 +6,13 @@ function build_models() {
     echo "Build Models: models will be generated and stored in models/.  Expect this to take several hours."
     echo
     echo "building User Collaborative Filtering Model..."
-#    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar createUCF model/UCF107_101.model data/UCF107_101_train 100
+#    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar createUCF model/UCF107_101.model data/$1 100
     echo "building Movie Collaborative Filtering Model..."
-#    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar createMCF model/MCF.model data/UCF107_101_train 100
+#    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar createMCF model/MCF.model data/$1 100
     echo "building Backwards User Collaborative Filtering Model..."
-#    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar createUCF model/BUCF.model data/UCF107_101_train 100 back
+#    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar createUCF model/BUCF.model data/$1 100 back
     echo "building Backwards Movie Collaborative Filtering Model..."
-#    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar createMCF model/BMCF.model data/UCF107_101_train 100 back
+#    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar createMCF model/BMCF.model data/$1 100 back
     echo
 }
 
@@ -21,13 +21,13 @@ function generate_ratings() {
     echo "Generating Ratings: ratings will be stored in data/.  Expect this to take at least an hour."
     echo
     echo "generating ratings for User Collaborative Filtering model"
-#    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar \test model/UCF107_101.model data/UCF107_101_test data/UCF107_101_test_result
+    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar \test model/UCF107_101.model data/$1 data/UCF107_101_test_result
     echo "generating ratings for Movie Collaborative Filtering model"
-#    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar \test model/MCF.model data/UCF107_101_test data/MCF107_101_test_result
+    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar \test model/MCF.model data/$1 data/MCF107_101_test_result
     echo "generating ratings for Backwards User Collaborative Filtering model"
-#    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar \test model/BUCF.model data/UCF107_101_test data/BUCF107_101_test_result
+    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar \test model/BUCF.model data/$1 data/BUCF107_101_test_result
     echo "generating ratings for Backwards Movie Collaborative Filtering model"
-#    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar \test model/BMCF.model data/UCF107_101_test data/BMCF107_101_test_result
+    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar \test model/BMCF.model data/$1 data/BMCF107_101_test_result
     echo
 }
 
@@ -36,13 +36,13 @@ function calculate_rmses() {
     echo "Calculating RMSEs.  This should be a relatively quick process."
     echo
     echo "RMSE for User Collaborative Filtering model:"
-    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar rmse data/UCF107_101_test data/UCF107_101_test_result
+    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar rmse data/$1 data/UCF107_101_test_result
     echo "RMSE for Movie Collaborative Filtering model:"
-    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar rmse data/UCF107_101_test data/MCF107_101_test_result
+    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar rmse data/$1 data/MCF107_101_test_result
     echo "RMSE for Backwards User Collaborative Filtering model:"
-    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar rmse data/UCF107_101_test data/BUCF107_101_test_result
+    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar rmse data/$1 data/BUCF107_101_test_result
     echo "RMSE for Backwards Movie Collaborative Filtering model:"
-    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar rmse data/UCF107_101_test data/BMCF107_101_test_result
+    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar rmse data/$1 data/BMCF107_101_test_result
     echo
 }
 
@@ -50,8 +50,8 @@ function combine_results() {
     echo
     echo "generating aggregate result file: data/combine_result.txt"
     java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar combine data/combine_result.txt data/UCF.log data/MCF.log data/BMCF.log data/BUCF.log
-    echo "RMSE for combined result:"
-    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar rmse data/UCF107_101_test data/combine_result.txt
+    echo "RMSE for combined result (ignore this value if running on test.txt):"
+    java -Xms1024M -Xmx3072M -jar bin/data_is_massive.jar rmse data/$1 data/combine_result.txt
     echo
 }
 
@@ -72,11 +72,14 @@ function usage() {
     echo "  combine:    combine ratings"
     echo "  rmse:       calculate the RMSEs"
     echo "  publish:    pubish result file for submission"
+    echo "  all:        execute full job"
     echo
 }
 
 
 # we will execute everything if passed no args, otherwise, execute only the requested function
+training_file="training.txt"
+test_file="test.txt"
 if [ $# -eq 0 ] 
 then
     build_models
@@ -84,20 +87,34 @@ then
     combine_results
     publish
 else
+    if [ $# -gt 1 ]
+    then
+        test_file=$2
+    fi
+    if [ $# -gt 2 ]
+    then
+        training_file=$3
+    fi
     if [ "$1" == "build" ]
     then
-        build_models
+        build_models $training_file
     elif [ "$1" == "rate" ]
     then
-        generate_ratings
+        generate_ratings $test_file
     elif [ "$1" == "combine" ]
     then
-        combine_results
+        combine_results $test_file
     elif [ "$1" == "rmse" ]
     then
-        calculate_rmses
+        calculate_rmses $test_file
     elif [ "$1" == "publish" ]
     then
+        publish
+    elif [ "$1" == "all" ]
+    then
+        build_models $training_file
+        generate_ratings $test_file
+        combine_results
         publish
     else
         usage
